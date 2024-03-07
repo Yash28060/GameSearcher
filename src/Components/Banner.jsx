d7dda615c1aa6c9770b30f04890c9e26e6ea840f
Yash28060 <yashkumarsaini2016@gmail.com>
2024-03-07 22:34:29 +0530

    Banner and Theme

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+
+function Banner({ gameBanner }) {
+  useEffect(() => {
+    // console.log("gameBanner", gameBanner);
+  }, []);
+
+  return (
+    <>
+      <div className="relative">
+        <div className="absolute p-5 bottom-0 bg-gradient-to-t from-slate-900 to-transparent w-full">
+          <h2 className=" text-[24px] text-white font-bold">
+            {gameBanner.name}
+          </h2>
+          <button className=" bg-blue-700 text-white px-2 p-1 rounded-xl hover:scale-95 ">
+            Get Now
+          </button>
+        </div>
+        <img
+          src={gameBanner.background_image}
+          className="md:h-[320px] w-full object-center  rounded-xl "
+        />
+      </div>
+    </>
+  );
+}
+
+export default Banner;
